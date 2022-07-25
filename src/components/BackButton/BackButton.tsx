b2f05d4f66a7e0fa0497943b58a465ea46b4ed60
Ianka Karuzia <iankakaruzia@gmail.com>
2022-07-25 20:09:57 -0300

    feat: add BackButton component

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router'
+import { classNames } from 'utils/styles/class-names'
+
+type BackButtonProps = {
+  theme?: 'light' | 'dark'
+}
+
+export function BackButton({ theme = 'light' }: BackButtonProps) {
+  const { back } = useRouter()
+
+  function goBack() {
+    back()
+  }
+
+  return (
+    <button onClick={goBack} className='flex items-center group'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='7' height='10'>
+        <path
+          fill='none'
+          fillRule='evenodd'
+          stroke={theme === 'light' ? '#4661E6' : '#CDD2EE'}
+          strokeWidth='2'
+          d='M6 9L2 5l4-4'
+        ></path>
+      </svg>
+      <span
+        className={classNames(
+          'ml-4 text-body3 font-bold group-hover:underline',
+          theme === 'light' ? 'text-gray-500' : 'text-white'
+        )}
+      >
+        Go Back
+      </span>
+    </button>
+  )
+}
