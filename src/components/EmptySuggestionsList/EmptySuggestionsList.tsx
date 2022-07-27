6c36aa4912e56ce461ded1eca5eb6f0d41d30ca1
Ianka Karuzia <iankakaruzia@gmail.com>
2022-07-27 18:44:53 -0300

    fix: tailwind tokens

--- src/components/EmptySuggestionsList/EmptySuggestionsList.tsx
+++ src/components/EmptySuggestionsList/EmptySuggestionsList.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function EmptySuggestionsList() {
   return (
-    <div className='bg-white rounded-brand my-8 mx-6 md:m-0 md:mt-6 py-18 md:py-24 px-6 md:px-36 flex flex-col items-center'>
+    <div className='bg-white rounded-xl my-8 mx-6 md:m-0 md:mt-6 py-18 md:py-24 px-6 md:px-36 flex flex-col items-center'>
       <div className='h-[108px] md:h-[136.74px] w-[102px] md:w-[129.64px] relative'>
         <Image
           src='/assets/suggestions/illustration-empty.svg'
@@ -12,10 +12,10 @@ export function EmptySuggestionsList() {
         />
       </div>
 
-      <h3 className='mt-[39px] mb-[14px] text-brand-h3 md:text-brand-h1 font-bold text-center text-brand-dark-300'>
+      <h3 className='mt-[39px] mb-[14px] text-heading3 md:text-heading1 font-bold text-center text-gray-700'>
         There is no feedback yet.
       </h3>
-      <p className='text-center text-brand-body3 md:text-brand-body1 mb-6 lg:mb-12 lg:max-w-[410px]'>
+      <p className='text-center text-body3 md:text-body1 mb-6 lg:mb-12 lg:max-w-[410px]'>
         Got a suggestion? Found a bug that needs to be squashed? We love hearing
         about new ideas to improve our app.
       </p>
