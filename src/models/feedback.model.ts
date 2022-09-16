45b32514392a50f90b66909a7e37e2afe85d5853
Ianka Karuzia <iankakaruzia@gmail.com>
2022-09-15 22:13:53 -0300

    feat: update packages

--- src/models/feedback.model.ts
+++ src/models/feedback.model.ts
@@ -6,8 +6,8 @@ import { feedbackOutput } from 'shared/outputs/feedback.outputs'
 const sortByEnum = z.enum(SORT_BY)
 const orderByEnum = z.enum(ORDER_BY)
 
-export type Category = z.infer<typeof feedbackOutput['_shape']['category']>
-export type Status = z.infer<typeof feedbackOutput['_shape']['status']>
+export type Category = z.infer<typeof feedbackOutput['shape']['category']>
+export type Status = z.infer<typeof feedbackOutput['shape']['status']>
 export type SortBy = z.infer<typeof sortByEnum>
 export type OrderBy = z.infer<typeof orderByEnum>
 
